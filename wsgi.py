7656c3e09bd7e254a336ce296a7019622ef68144
anh do <anhdo@anhs-MacBook-Pro.local>
2019-07-23 22:54:30 +0700

    resture master

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
-from wine import wine_app
-from wine.dash_app import create_dash_app
+from wine_quality import wine_app
+from wine_quality.dash_app import create_dash_app
 dash_app = create_dash_app(wine_app.create_app())
 app = dash_app.server
 if __name__ == "__main__":
